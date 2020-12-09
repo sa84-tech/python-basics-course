9a38f980232b4f7439f4688f620c5e16a7463ea6
sa84 <sa84@list.ru>
2020-12-10 01:46:02 +0300

    task6 updated

--- storage.py
+++ storage.py
@@ -62,17 +62,20 @@ class Storage:
         return False
 
     def get_equipment_by_sn(self, eq_sn):
-        print(eq_sn)
         for item in self.__stored_equipment:
-            # print(item)
             if eq_sn == item.get('equipment').sn:
                 return item.get('equipment')
 
-    def get_equipments_by_name(self):
-        pass
-
-    def get_equipments_by_type(self):
-        pass
+    def get_equipments_by_name(self, eq_name, qtty=0):
+        equipments = []
+        count = 0
+        for item in self.__stored_equipment:
+            if eq_name == item.get('equipment').name:
+                count += 1
+                equipments.append(item.get('equipment'))
+                if qtty == count:
+                    break
+        return equipments
 
     def get_equipment_qtty(self, eq):
         return len([e.get('equipment').name for e in self.__stored_equipment if e.get('equipment').name == eq.name])
@@ -80,16 +83,13 @@ class Storage:
     def get_equipments_list(self, eq_type='', eq_name='', eq_sn='', full=False):
         n = 0
         if full:
-            return [[n := n + 1, (e := se.get('equipment')).eq_type, e.name, e.sn] for se in self.__stored_equipment
+            return [[n := n + 1, (e := se.get('equipment')).eq_type, e.name, se.get('receipt_date'), e.sn]
+                    for se in self.__stored_equipment
                     if (not eq_type or eq_type == se.get('equipment').eq_type) and (not eq_name) and (not eq_sn)]
         else:
-            d = [e.get('equipment').name for e in self.__stored_equipment]
-            # print(d)
-            # di = [{e.get('equipment').name: e.get('equipment').eq_type} for e in self.__stored_equipment]
-            # print(di)
-            dic = Counter(d)
-            # print(dic)
-            return [[i, key, dic.get(key)] for i, key in enumerate(dic.keys(), 1)]
+            dit = Counter([tuple([e.get('equipment').name, e.get('equipment').eq_type])
+                           for e in self.__stored_equipment])
+            return [[i, key[1], key[0], int(dit.get(key))] for i, key in enumerate(dit.keys(), 1)]
 
     def take_equipment(self, *equipment):
         err_count = 0
@@ -109,7 +109,27 @@ class Storage:
         return 'ok' if not err_count else f'Ошибка. Некореткный тип данных, ' \
                                           f'принято оборудования: {len(equipment) - err_count} из {len(equipment)}'
 
+    def give_equipment(self, to, *equipments):
+        count = 0
+        for cur_eq in equipments:
+            for i, stored_item in enumerate(self.__stored_equipment):
+                if cur_eq == stored_item.get('equipment'):
+                    self.__delivered_equipment.append(tmp := {
+                        'id': stored_item.get('id'),
+                        'equipment': cur_eq,
+                        'storage': to,
+                        'receipt_date': '',
+                        'delivered_date': datetime.datetime.now()
+                    })
+                    self.__journal.append(tmp)
+                    self.__stored_equipment.pop(i)
+                    self.__qtty_info[cur_eq.eq_type] -= 1
+                    count += 1
+                    break
+        return 'ok' if count == (l := len(equipments)) else f'Ошибка. Передано оборудования: {count} из {l}'
+
     def deliver_equipment(self, to, equipment, qtty=1):
+        """ deprecated """
         if self.qtty_validate(equipment, qtty):
             for _ in range(qtty):
                 for i, stored_item in enumerate(self.__stored_equipment):
@@ -130,9 +150,16 @@ class Storage:
         else:
             return 'Ошибка: На складе отсутствует оборудование в указанном количесте.'
 
-    def display_equipments_list(self, eq_type='', eq_name='', eq_sn=''):
-        table = PrettyTable(['№ п/п', 'Тип', 'Наименоваие', 'Серийный номер'])
-        table.add_rows(self.get_equipments_list(eq_type, eq_name, eq_sn, full=True))
+    def display_equipments_list(self, eq_type='', eq_name='', eq_sn='', full=False):
+        if full:
+            table = PrettyTable(['№ п/п', 'Тип', 'Наименоваие', 'Дата поступления', 'Серийный номер'])
+            li = [[it[0], it[1], it[2], it[3].strftime("%d.%m.%Y %H:%M:%S"), it[4]]
+                  for it in self.get_equipments_list(eq_type, eq_name, eq_sn, full)]
+            table.add_rows(li)
+
+        else:
+            table = PrettyTable(['№ п/п', 'Тип', 'Наименоваие', 'Количество'])
+            table.add_rows(self.get_equipments_list(eq_type, eq_name, eq_sn, full))
         print(table)
 
     def display_report(self, report_type, full=False):
@@ -147,22 +174,10 @@ class Storage:
                   f'{str(table)}\nОбщее количество оборудования: {len(self.__stored_equipment)}')
 
         elif report_type == 'storage_report':
-            span = 'Общий'
-
-            if full:
-                span = 'Подробный'
-                table = PrettyTable(['№ п/п', 'Тип', 'Наименование', 'Серийный номер', 'Дата Поступления'])
-                for i, el in enumerate(self.__stored_equipment, 1):
-                    eq = el.get("equipment")
-                    table.add_row([i, eq.eq_type, eq.name, eq.sn, el.get("receipt_date").strftime('%d.%m.%Y %H:%M:%S')])
-            else:
-                table = PrettyTable(['№ п/п', 'Наименование', 'Количество'])
-                di = Counter([e.get('equipment').name for e in self.__stored_equipment])
-                for i, key in enumerate(di.keys(), 1):
-                    table.add_row([i, key, di.get(key)])
-
+            span = 'Подробный' if full else 'Общий'
             print(f'\nОтчет - Анализ запасов ({span}):\nСклад: {self.storage_name}, '
-                  f'Дата: {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}\n{str(table)}')
+                  f'Дата: {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}')
+            self.display_equipments_list(full=full)
 
         elif report_type == 'delivered_equipment_report':
             table = PrettyTable(['№ п/п', 'Тип', 'Наименование', 'Место Установки', 'Дата Отгрузки'])
@@ -171,7 +186,7 @@ class Storage:
                 table.add_row([i, el.get("equipment").eq_type, el.get("equipment").name,
                                el.get('storage'), el.get("delivered_date").strftime("%d.%m.%Y %H:%M:%S")])
 
-            print(f'\nОтчет - Отгрузка Товара.\nСклад: {self.storage_name}, '
+            print(f'\nОтчет - Отгруженное Оборудование.\nСклад: {self.storage_name}, '
                   f'Дата: {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}\n{str(table)}')
 
         elif report_type == 'movement_report':
--- task6.py
+++ task6.py
@@ -1,18 +1,9 @@
 # pip install PrettyTable
 import uuid
 import pickle
-from prettytable import PrettyTable
 
 from equipment import Printer, Scanner, Copier
 from storage import Storage
-
-try:
-    with open("storage.pkl", "rb") as fp:
-        storage = pickle.load(fp)
-except (FileNotFoundError, EOFError):
-    storage = Storage('Центральный')
-
-print(storage)
 
 
 def take_equipment(_storage):
@@ -47,7 +38,7 @@ def take_equipment(_storage):
     return
 
 
-def deliver_equipment(_storage):
+def give_equipment(_storage):
     d = {1: 'Принтер', 2: 'Сканер', 3: 'Копир', 4: ''}
     d_menu_item = 0
     eq_list = []
@@ -68,87 +59,104 @@ def deliver_equipment(_storage):
             d_menu_item = 0
             print('Некоректный ввод')
 
-    equipment = _storage.get_equipment_by_sn(eq_list[eq_num - 1][3])
+    equipment = _storage.get_equipment_by_sn(eq_list[eq_num - 1][4])
     print(equipment)
-    print(_storage.deliver_equipment(eq_to, equipment))
+    print(_storage.give_equipment(eq_to, equipment))
 
 
-def bulk_deliver_equipment(_storage):
-    d = {1: 'Принтер', 2: 'Сканер', 3: 'Копир', 4: ''}
-    d_menu_item = 0
-    eq_list = []
-    _storage.display_report('storage_report')
-    # _storage.display_equipments_list()
-
+def bulk_give_equipment(_storage):
+    _storage.display_equipments_list()
     eq_list = _storage.get_equipments_list()
-    # eq_num = int(input('Выберете номер устройства: '))
-    # eq_qtty = int(input('Укажите количество: '))
-    # eq_to = input('Укажите место установки: ')
-    print(eq_list)
 
-    # while True:
-    #
-    #     try:
-    #         d_menu_item = int(input('(1) Принтер | (2) Сканер | (3) Копир | (4) Все | (5) Отмена\n'
-    #                                 'Укажите тип оборудования: '))
-    #         if d_menu_item == 5:
-    #             return
-    #
-    #         _storage.display_equipments_list(d.get(d_menu_item))
-    #
-    #     except ValueError:
-    #         pass
+    while True:
+        try:
+            eq_num = int(input('Выберете номер устройства: '))
+            if eq_num < 0 or eq_num > len(eq_list):
+                raise ValueError
+            eq_qtty = int(input('Укажите количество, 0 - все: '))
+            if eq_qtty > eq_list[eq_num-1][3]:
+                raise ValueError
+            eq_to = input('Укажите место установки: ')
+            if len(eq_to) < 2:
+                raise ValueError
+        except ValueError:
+            print('Некорректное значение')
+        else:
+            break
+
+    equipments = _storage.get_equipments_by_name(eq_list[eq_num - 1][2], eq_qtty)
+    print(_storage.give_equipment(eq_to, *equipments))
 
 
 def display_report(_storage):
-    report_menu = '\nОтчеты.\n(1) Оборудование | (2) Анализ Запасов | ' \
-                  '(3) Отгрузка Товаров | (4) Движение ТМЦ | (5) Назад'
+    report_menu = '\n*** ОТЧЕТЫ ***\n(1) Оборудование | (2) Анализ Запасов | ' \
+                  '(3) Отгруженное Оборудование | (4) Движение ТМЦ | (5) Назад'
 
     while True:
         print(report_menu)
         r_menu_item = input('Выберете пункт меню: ')
 
         if r_menu_item == '1':
-            storage.display_report('qtty_report')
+            _storage.display_report('qtty_report')
 
         elif r_menu_item == '2':
             r_menu_item = '0'
             while r_menu_item != '2' and r_menu_item != '1':
                 print('(1) Общий | (2) Подробный')
                 r_menu_item = input('Выберете пункт меню: ')
-            storage.display_report('storage_report', True if r_menu_item == '2' else False)
+            _storage.display_report('storage_report', True if r_menu_item == '2' else False)
 
         elif r_menu_item == '3':
-            storage.display_report('delivered_equipment_report')
+            _storage.display_report('delivered_equipment_report')
 
         elif r_menu_item == '4':
-            storage.display_report('movement_report')
+            _storage.display_report('movement_report')
 
         elif r_menu_item == '5':
             break
 
 
-while True:
-    main_menu = 'Главное меню.\n(1) Принять оборудование | ' \
-                '(2) Отгрузить оборудование | (3) Отчеты | (4) Выход'
+def main():
+    try:
+        with open("storage.pkl", "rb") as fp:
+            storage = pickle.load(fp)
+    except (FileNotFoundError, EOFError):
+        storage = Storage('Центральный')
 
-    print(main_menu)
+    print(storage)
 
-    menu_item = input('Выберете пункт меню: ')
+    while True:
+        print('\n*** ГЛАВНОЕ МЕНЮ ***\n(1) Принять оборудование | '
+              '(2) Переместить оборудование | (3) Отчеты | (4) Диагностика | (5) Выход')
 
-    if menu_item == '1':
-        take_equipment(storage)
+        menu_item = input('Выберете пункт меню: ')
+        if menu_item == '1':
+            take_equipment(storage)
 
-    elif menu_item == '2':
-        bulk_deliver_equipment(storage)
+        elif menu_item == '2':
+            answer = ''
+            while answer != '3':
+                print('(1) Перемещение определенного оборудования | (2) Групповое перемещение | (3) Назад')
+                answer = input('Выберете пункт меню: ')
+                if answer == '1':
+                    give_equipment(storage)
+                elif answer == '2':
+                    bulk_give_equipment(storage)
 
-    elif menu_item == '3':
+        elif menu_item == '3':
+            display_report(storage)
 
-        display_report(storage)
+        elif menu_item == '4':
+            print('Диагностика..')
 
-    elif menu_item == '4':
-        print('Выход из программы..')
-        break
+        elif menu_item == '5':
+            print('Выход из программы..')
+            break
 
-with open("storage.pkl", "wb") as fp:
-    pickle.dump(storage, fp)
+    with open("storage.pkl", "wb") as fp:
+        pickle.dump(storage, fp)
+
+    return 0
+
+
+main()
